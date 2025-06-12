f947c7655fa9325a69000880b6bf61c6a36dadbe
ahscorp <ahscorpforgithub@gmail.com>
2025-06-12 15:37:36 +0530

    sheet test 3

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -235,7 +235,7 @@ const ApplicationForm = ({ isOpen, onClose, job }: ApplicationFormProps) => {
         .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(webhookPayload[key]))
         .join('&');
       
-      const googleScriptUrl = "https://script.google.com/macros/s/AKfycbzesKj5CzNT3KWmNY_-OoVPn36GwS5lc20OvGK3VjvaWraZhqwZ2iF49i0lmOhcbBjN/exechttps://script.google.com/macros/s/AKfycby4dkwDr3suI-QekiOdzOyS6hJ1EYe1iimvTwvHQyE/devhttps://script.google.com/macros/s/AKfycbwL_RX_t9EfypFFEK6bo7IUKOnSlHG-ZujIV8dw47tIGo-ivrIXWgpYRkCSy13e-bZY/exechttps://script.google.com/macros/s/AKfycbxnaR-lK3qmWhxWQz_YRQuwsSgbjUZydE8hBkNoQr6abAPDkI6xrBRKb1Iclor2DjAXNg/exec";
+      const googleScriptUrl = "https://script.google.com/macros/s/AKfycbxcgspOAM8bn8cbQK7mSXPkiaSoWxhw93PSMIJwAofgU9nbG6HOm2K0Rk8C3A8G0aCJKA/exec";
       
       try {
         const response = await fetch(googleScriptUrl, {
