0f464d6583f786169e89d2de728079fd21cd91b0
ahscorp <ahscorpforgithub@gmail.com>
2025-06-12 15:01:28 +0530

    sheet changed

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -235,7 +235,7 @@ const ApplicationForm = ({ isOpen, onClose, job }: ApplicationFormProps) => {
         .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(webhookPayload[key]))
         .join('&');
       
-      const googleScriptUrl = "https://script.google.com/macros/s/AKfycbyNvAzmXRqzobEsDtaiSnfd0Wso3e2rK-Vyb3wIbFwlDrtSgnfIPhNym8_mI_VF2IRsGQ/exec";
+      const googleScriptUrl = "https://script.google.com/macros/s/AKfycbzesKj5CzNT3KWmNY_-OoVPn36GwS5lc20OvGK3VjvaWraZhqwZ2iF49i0lmOhcbBjN/exec";
       
       try {
         const response = await fetch(googleScriptUrl, {
