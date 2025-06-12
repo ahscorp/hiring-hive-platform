b8af36a8f55e00e2b31a8e23ae2962c505d04caa
ahscorp <ahscorpforgithub@gmail.com>
2025-06-12 16:03:22 +0530

    sheet test 8

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -235,7 +235,7 @@ const ApplicationForm = ({ isOpen, onClose, job }: ApplicationFormProps) => {
         .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(webhookPayload[key]))
         .join('&');
       
-      const googleScriptUrl = "https://script.google.com/macros/s/AKfycbxcgspOAM8bn8cbQK7mSXPkiaSoWxhw93PSMIJwAofgU9nbG6HOm2K0Rk8C3A8G0aCJKA/exechttps://script.google.com/macros/s/AKfycbzac6YyvN-ZdkSkDdxb1LwTTvDwE577YC4HybYiaEXvYypEYJS9i0gXPOr6mZHbIk7Y/exechttps://script.google.com/macros/s/AKfycbyP3SaQqidJe5K6_haYn7baDMYeiFZCmukPMIkGCik/devhttps://script.google.com/macros/s/AKfycbyNvAzmXRqzobEsDtaiSnfd0Wso3e2rK-Vyb3wIbFwlDrtSgnfIPhNym8_mI_VF2IRsGQ/exechttps://script.google.com/macros/s/AKfycbyDLsekqyOQ_cRbQdOMmikbcSYLh7Ho5_pIZg6kn2LB/dev";
+      const googleScriptUrl = "https://script.google.com/macros/s/AKfycbxcgspOAM8bn8cbQK7mSXPkiaSoWxhw93PSMIJwAofgU9nbG6HOm2K0Rk8C3A8G0aCJKA/exechttps://script.google.com/macros/s/AKfycbzac6YyvN-ZdkSkDdxb1LwTTvDwE577YC4HybYiaEXvYypEYJS9i0gXPOr6mZHbIk7Y/exechttps://script.google.com/macros/s/AKfycbyP3SaQqidJe5K6_haYn7baDMYeiFZCmukPMIkGCik/devhttps://script.google.com/macros/s/AKfycbyNvAzmXRqzobEsDtaiSnfd0Wso3e2rK-Vyb3wIbFwlDrtSgnfIPhNym8_mI_VF2IRsGQ/exechttps://script.google.com/macros/s/AKfycbyDLsekqyOQ_cRbQdOMmikbcSYLh7Ho5_pIZg6kn2LB/devhttps://script.google.com/macros/s/AKfycbyOiU2PNyJdmA4GgINt209mcNQRRKtJ-336eZ2c7xIej-lwUVeS9Qk0FGIOmL6YJS4F/exec";
       
       try {
         const response = await fetch(googleScriptUrl, {
